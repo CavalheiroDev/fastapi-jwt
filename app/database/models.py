14d9a024955b1f6586b6524c8ed28c630e169fd8
Gabriel Cavalheiro <ga3-cavalheiro@hotmail.com>
2021-08-29 17:09:13 -0300

    Modificando o tipo da coluna do password e adicionando o email como um atributo unico.

--- app/database/models.py
+++ app/database/models.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, Integer, Text, VARCHAR
-from sqlalchemy_utils import PasswordType
 from app.database.events import Base
 
 
@@ -9,14 +8,8 @@ class User(Base):
 
     id = Column('id', Integer, primary_key=True, unique=True, nullable=False, autoincrement=True)
     fullname = Column('nome', Text(125), nullable=False)
-    email = Column('email', VARCHAR(125), nullable=False)
-    password = Column('senha', PasswordType(
-            schemes=[
-                'pbkdf2_sha512',
-                'md5_crypt'
-            ],
-            deprecated=['md5_crypt']), 
-            nullable=False)
+    email = Column('email', VARCHAR(125), nullable=False, unique=True)
+    password = Column('senha', VARCHAR(125), nullable=False)
 
     def __init__(self, fullname, email, password):
         self.fullname = fullname
