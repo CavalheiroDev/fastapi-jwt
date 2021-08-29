9d64c01292cb0cc3faaa0edd485cd1a93cef673a
Gabriel Cavalheiro <ga3-cavalheiro@hotmail.com>
2021-08-29 17:06:39 -0300

    Adicionando o orm mode nos schemas.

--- app/api/schemas.py
+++ app/api/schemas.py
@@ -1,12 +1,13 @@
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field
 
 
 class UserSchema(BaseModel):
     fullname: str = Field(...)
-    email: EmailStr = Field(...)
+    email: str = Field(...)
     password: str = Field(..., min_length=6)
 
     class Config:
+        orm_mode = True
         schema_extra = {
             'fullname': 'Gabriel Cavalheiro Barreto',
             'email': 'gabriel.cavalheiro-barreto@teste.com.br',
@@ -15,10 +16,11 @@ class UserSchema(BaseModel):
 
 
 class UserLoginSchema(BaseModel):
-    email: str = EmailStr(...)
+    email: str = Field(...)
     password: str = Field(..., min_length=6)
 
     class Config:
+        orm_mode = True
         schema_extra = {
             'email': 'gabriel.cavalheiro-barreto@teste.com.br',
             'password': 'senhasupersecreta1234' 
